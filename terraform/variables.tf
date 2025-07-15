ef1cc24e93c37b9b519b269a2c3eee933a63e0c6
mukeshyadav143 <114664443+mukeshyadav143@users.noreply.github.com>
2025-07-15 11:34:50 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "20.20.0.0/16"
 }
 variable "aws_region" {
   description = "AWS Region"
